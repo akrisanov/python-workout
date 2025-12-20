0470d79043896fc30031522981db15b8e06f1462
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-20 23:45:26 +0300

    (solution): extend Guessing Game with checking number of chances

--- ch02/01_guessing_game.py
+++ ch02/01_guessing_game.py
@@ -5,10 +5,12 @@ logger = logging.getLogger(__name__)
 
 
 def guessing_game():
-    answer = random.randint(0, 100)
+    answer = random.randint(1, 100)
+    guesses = 0
+
     logger.debug(f"The target number is {answer}")
 
-    while True:
+    while guesses < 3:
         try:
             guess_str = input("Guess a number: ").strip()
             guess = int(guess_str)
@@ -19,13 +21,18 @@ def guessing_game():
             print("\nBye!")
             return
 
+        guesses += 1
+
+        if guess == answer:
+            print("Just right")
+            break
+
         if guess > answer:
             print("Too high")
         elif guess < answer:
             print("Too low")
-        else:
-            print("Just right")
-            break
+    else:
+        print("You didn't guess in time")
 
 
 if __name__ == "__main__":
