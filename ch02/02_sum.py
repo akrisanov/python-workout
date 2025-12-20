2bc8543347a0a0dfe198fefdff7b442f1895dfa6
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-21 00:36:40 +0300

    (solution): avg function for sequences

--- ch02/02_sum.py
+++ ch02/02_sum.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
 
 
 def mysum(nums: Iterable[float], start: float = 0.0) -> float:
@@ -8,7 +8,12 @@ def mysum(nums: Iterable[float], start: float = 0.0) -> float:
     return result
 
 
+def avg(nums: Sequence[float]) -> float:
+    return mysum(nums) / len(nums)
+
+
 if __name__ == "__main__":
     print(mysum([], 15))
     print(mysum([1, 2, 3], 10))
     print(mysum((15, 15, 24)))
+    print(avg([10, 10, 10]))
