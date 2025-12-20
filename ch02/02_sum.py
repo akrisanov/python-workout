cb9b27526a49da0054a8dbe4c56832716704e04b
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-21 01:00:46 +0300

    (solution): stats function for list of words

--- ch02/02_sum.py
+++ ch02/02_sum.py
@@ -1,15 +1,25 @@
 from collections.abc import Iterable, Sequence
 
+Number = int | float
 
-def mysum(nums: Iterable[float], start: float = 0.0) -> float:
+
+def mysum(nums: Iterable[Number], start: float = 0.0) -> float:
     result = float(start)
     for num in nums:
         result += num
     return result
 
 
-def avg(nums: Sequence[float]) -> float:
+def avg(nums: Sequence[Number]) -> float:
     return mysum(nums) / len(nums)
+
+
+def stats(words: list[str]) -> tuple[int, int, float]:
+    if not words:
+        return 0, 0, 0
+
+    lengths = list(map(len, words))
+    return min(lengths), max(lengths), avg(lengths)
 
 
 if __name__ == "__main__":
@@ -17,3 +27,5 @@ if __name__ == "__main__":
     print(mysum([1, 2, 3], 10))
     print(mysum((15, 15, 24)))
     print(avg([10, 10, 10]))
+    print(stats(["hello", "world", "me", "all", "Python"]))
+    print(stats([]))
