c92cddabe66e4b37435003199d6152a394a12635
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-21 01:19:54 +0300

    (solution): sum function with skipping non like numbers

--- ch02/02_sum.py
+++ ch02/02_sum.py
@@ -1,13 +1,29 @@
 from collections.abc import Iterable, Sequence
+from typing import Any
 
 Number = int | float
 
 
 def mysum(nums: Iterable[Number], start: float = 0.0) -> float:
-    result = float(start)
+    total = float(start)
     for num in nums:
-        result += num
-    return result
+        total += num
+    return total
+
+
+def sum_obj(objects: Iterable[Any], start: float = 0.0) -> float:
+    total = float(start)
+
+    for obj in objects:
+        if isinstance(obj, bool):
+            continue
+
+        try:
+            total += int(obj)
+        except (TypeError, ValueError):
+            pass
+
+    return total
 
 
 def avg(nums: Sequence[Number]) -> float:
@@ -16,7 +32,7 @@ def avg(nums: Sequence[Number]) -> float:
 
 def stats(words: list[str]) -> tuple[int, int, float]:
     if not words:
-        return 0, 0, 0
+        return 0, 0, 0.0
 
     lengths = list(map(len, words))
     return min(lengths), max(lengths), avg(lengths)
@@ -26,6 +42,11 @@ if __name__ == "__main__":
     print(mysum([], 15))
     print(mysum([1, 2, 3], 10))
     print(mysum((15, 15, 24)))
+
     print(avg([10, 10, 10]))
+
     print(stats(["hello", "world", "me", "all", "Python"]))
     print(stats([]))
+
+    print(sum_obj([]))
+    print(sum_obj([1, "1", 0, "4", [1, 2, 3], 5], 10))
