eea71742c5ceb1123c0b4363f95e1647e3a50817
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-21 00:32:09 +0300

    (solution): Extend sum function with the start argument

--- ch02/02_sum.py
+++ ch02/02_sum.py
@@ -1,9 +1,14 @@
-def mysum(*nums: float) -> float:
-    result = 0.0
+from collections.abc import Iterable
+
+
+def mysum(nums: Iterable[float], start: float = 0.0) -> float:
+    result = float(start)
     for num in nums:
         result += num
     return result
 
 
 if __name__ == "__main__":
-    print(mysum(*[1, 2, 3]))
+    print(mysum([], 15))
+    print(mysum([1, 2, 3], 10))
+    print(mysum((15, 15, 24)))
