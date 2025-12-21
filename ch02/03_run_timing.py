0da9bbaca4cfd6d96507e310b0ba4fa62f3144d7
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-21 14:57:09 +0300

    (solution) take digits from a float number

--- ch02/03_run_timing.py
+++ ch02/03_run_timing.py
@@ -17,6 +17,21 @@ def run_timing() -> tuple[float, int]:
     return avg_time, runs
 
 
+def take_float(number: float, before: int, after: int) -> float:
+    sign = -1 if number < 0 else 1
+    integer_part = int((abs(number)))
+    last_int_digits = integer_part % (10**before)
+
+    fractional_part = abs(number) - integer_part
+    shifted = fractional_part * (10**after)
+    first_fr_digits = int(shifted + 1e-12) / 10**after
+
+    return sign * (last_int_digits + first_fr_digits)
+
+
 if __name__ == "__main__":
-    avg_time, runs = run_timing()
-    print(f"Average of {avg_time:.2f}, over {runs} runs")
+    # avg_time, runs = run_timing()
+    # print(f"Average of {avg_time:.2f}, over {runs} runs")
+
+    print(take_float(1234.5678, 2, 3))
+    print(take_float(-1234.5678, 2, 3))
