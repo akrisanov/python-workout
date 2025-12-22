047ea488ecff13b8b3850e13c1a2ebcdc8da443c
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-22 21:51:11 +0300

    (solution) Pig Latin: Handling capitalized words

--- ch03/01_pig_latin.py
+++ ch03/01_pig_latin.py
@@ -2,10 +2,13 @@ def pig_latin(word: str) -> str:
     if not word:
         return word
 
-    if word[0] in "aeiou":
-        return f"{word}way"
+    pig_word = f"{word}way" if word[0].lower() in "aeiou" else f"{word[1:]}{word[0]}ay"
 
-    return f"{word[1:]}{word[0]}ay"
+    is_capitalized = word[:1].isupper() and word[1:].islower()
+    if is_capitalized:
+        return pig_word[:1].upper() + pig_word[1:].lower()
+
+    return pig_word
 
 
 if __name__ == "__main__":
@@ -13,3 +16,4 @@ if __name__ == "__main__":
     print(pig_latin("eat"))
     print(pig_latin("python"))
     print(pig_latin("computer"))
+    print(pig_latin("Hello"))
