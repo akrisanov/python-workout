af61e83511be544b61213096b969bd5337c23940
Andrey Krisanov <238505+akrisanov@users.noreply.github.com>
2025-12-22 22:13:27 +0300

    (solution) Pig Latin: Handling punctuation

--- ch03/01_pig_latin.py
+++ ch03/01_pig_latin.py
@@ -1,19 +1,32 @@
+import string
+
+
 def pig_latin(word: str) -> str:
     if not word:
         return word
 
-    pig_word = f"{word}way" if word[0].lower() in "aeiou" else f"{word[1:]}{word[0]}ay"
+    is_capitalized = word[0].isupper() and word[1:].islower()
+    punctuation = ""
 
-    is_capitalized = word[:1].isupper() and word[1:].islower()
+    if word[-1] in string.punctuation:
+        punctuation = word[-1]
+        word = word[:-1]
+
+    if word[0].lower() in "aeiou":
+        pig_word = f"{word}way"
+    else:
+        pig_word = f"{word[1:]}{word[0]}ay"
+
     if is_capitalized:
-        return pig_word[:1].upper() + pig_word[1:].lower()
+        pig_word = pig_word[0].upper() + pig_word[1:].lower()
 
-    return pig_word
+    return pig_word + punctuation
 
 
 if __name__ == "__main__":
     print(pig_latin("air"))
     print(pig_latin("eat"))
     print(pig_latin("python"))
-    print(pig_latin("computer"))
-    print(pig_latin("Hello"))
+    print(pig_latin("Python?"))
+    print(pig_latin("computer."))
+    print(pig_latin("Hello!"))
